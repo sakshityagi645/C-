7febef78c4e506c1faa124d9f7797b528252e352
Sakshi Tyagi <55343037+sakshityagi645@users.noreply.github.com>
2021-12-27 18:50:03 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Enum
+{
+    enum Level
+    {
+        Low,
+        Medium,
+        High
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Level myVar = Level.Low;
+            Console.WriteLine(myVar);
+        }
+    }
+}
