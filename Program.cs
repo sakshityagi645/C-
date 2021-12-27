836d6c73e59d2310cd75f313533e16d8262eb0de
Sakshi Tyagi <55343037+sakshityagi645@users.noreply.github.com>
2021-12-27 18:38:59 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace arrayMismatchException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[5] { "car","animals","birds","plants","humans"};
+            object[] objs = (object[])array;
+
+
+            try
+            {
+                Object obj = (Object)13;
+                objs[2] = obj;
+                foreach (object Name in objs)
+                {
+                    System.Console.WriteLine(Name);
+                }
+            }
+            catch(ArrayTypeMismatchException e)
+            {
+                Console.WriteLine("Error occured: " + e.Message);
+            }
+        }
+    }
+}
