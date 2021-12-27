2661f5e97131c1ac470f205c7bc68384b1886fba
Sakshi Tyagi <55343037+sakshityagi645@users.noreply.github.com>
2021-12-27 18:58:14 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            int res = 1;
+            while (n != 1)
+            {
+                res = res * n;
+                n = n - 1;
+            }
+            Console.WriteLine("Factorial of number is: " + res);
+        }
+    }
+}
