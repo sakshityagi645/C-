c6fc50ec8f222d70e6eca1a4e5b656080e154591
Sakshi Tyagi <55343037+sakshityagi645@users.noreply.github.com>
2021-12-27 16:22:24 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DivideByZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, num2, div;
+            num = 30;
+            num2 = 0;
+            try {
+                div = num / num2;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error message is: " + e.Message);
+            }
+
+        }
+    }
+}
