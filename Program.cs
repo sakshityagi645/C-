ca78af029f3fec54b8cb1467c75d2ca2992a1630
Sakshi Tyagi <55343037+sakshityagi645@users.noreply.github.com>
2021-12-27 18:40:03 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BreakContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5]{1,2,3,4,5};
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i < 3)
+                {
+                    Console.WriteLine("print continue.....");
+                    continue;
+                }
+                Console.WriteLine("Print My name is Sakshi...");
+            }    
+            
+            //int a = 0;
+            //while (a < 20)
+            //{
+            //    Console.WriteLine("value of a: {0}", a);
+            //    a++;
+
+            //    if (a > 15)
+            //    {
+            //        break;
+            //    }
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
