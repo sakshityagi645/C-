cb62b1b1b2c5d0316f91971b16592eec85536ec0
Sakshi Tyagi <55343037+sakshityagi645@users.noreply.github.com>
2021-12-27 18:55:11 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MaximumMinimumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[8] { 12,34,64,21,100,45,1,7};
+            int max, min;
+            max = array[0];
+            min = array[0];
+            for (int i = 0; i < array.GetLength(0); i++) {
+                if (array[i] > max)
+                { 
+                    max = array[i]; 
+                }
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+            Console.WriteLine("Maximum element is : " + max);
+            Console.WriteLine("Minimum element is : " + min);
+        }
+    }
+}
