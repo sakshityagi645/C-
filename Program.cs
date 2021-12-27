7a86c59cdd2ad76376d9222fe3ce53accbf8abbb
Sakshi Tyagi <55343037+sakshityagi645@users.noreply.github.com>
2021-12-27 23:44:20 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CustomException
+{
+    class Program
+    {
+        static void checkAge(int age)
+        {
+            if (age < 18)
+            {
+                throw new ArithmeticException("Access denied");
+            }
+            else
+            {
+                Console.WriteLine("Access granted");
+            }
+        }
+       public static void Main(string[] args)
+        {
+            try
+            {
+                checkAge(15);
+            }
+            catch(ArithmeticException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
