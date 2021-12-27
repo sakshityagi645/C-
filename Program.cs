f83f3f5eab3d3b4e4a18f1aa8855da0412519e5a
Sakshi Tyagi <55343037+sakshityagi645@users.noreply.github.com>
2021-12-27 18:52:58 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NegativeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[7]
+            {
+                12,2,-2,45,-45,-76,58
+            };
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+
+                if (array[i] < 0)
+                {
+                   Console.WriteLine("Negative numbers in array is: " + " " + array[i]);
+                }
+            }          
+        }
+    }
+ }
