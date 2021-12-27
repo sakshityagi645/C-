e79e4695702c908cb30cefbf5c777a63b169db09
Sakshi Tyagi <55343037+sakshityagi645@users.noreply.github.com>
2021-12-27 23:42:02 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿//using System;
+
+//class NumbersFromOneToN
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("Please write your number: ");
+//        int n = int.Parse(Console.ReadLine());
+
+//        for (int i = 1; i <= n; i++)
+//        {
+//            Console.Write("{0} ", i);
+//        }
+//        Console.WriteLine();
+//    }
+//}
+using System;
+using System.Collections.Generic;
+
+namespace BinaryNumbers
+{
+    class Program
+    {
+        void binaryconversion()
+        {
+            int n, i;
+            int[] a = new int[20];
+            Console.Write("Enter the number to convert: ");
+            n = int.Parse(Console.ReadLine());
+            for (i = 0; n > 0; i++)
+            {
+                a[i] = n % 2;
+                n = n / 2;
+            }
+            Console.Write("Binary of the given number= ");
+
+            for (i = i - 1; i >= 0; i--)
+            {
+                Console.Write(a[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program binary = new Program();
+            binary.binaryconversion();
+            Queue<int> number = new Queue<int>();
+
+            number.Enqueue(1);
+            number.Enqueue(2);
+            number.Enqueue(3);
+            number.Enqueue(4);
+
+            //foreach (int name in number)
+            //{
+            //    Console.WriteLine("Elements of Queue is: " + number);
+            //}
+
+            //Console.WriteLine("Peek element: " + names.Peek());
+            //Console.WriteLine("Dequeue: " + names.Dequeue());
+            //Console.WriteLine("After Dequeue, Peek element: " + names.Peek());
+            //Console.ReadLine();
+        }
+    }
+}
