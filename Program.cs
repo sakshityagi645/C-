8599c70ad5978eba3760bdf767397a8077869f46
Sakshi Tyagi <55343037+sakshityagi645@users.noreply.github.com>
2021-12-27 16:04:53 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IndexOutOfRangeException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5] { 1, 2, 3, 4, 5 };
+            try
+            {
+                Console.WriteLine(array[10]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error message is: " + e.Message);
+            }
+        }
+    }
+}
