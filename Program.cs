5e454d36ad9cd87ec00de391b7ad5af2536bb86b
Sakshi Tyagi <55343037+sakshityagi645@users.noreply.github.com>
2021-12-28 15:31:16 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i;
+            Console.Write("Enter the number: ");
+            n = int.Parse(Console.ReadLine());
+            for (i = 2; i < 100; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine("Not Prime.");
+                   break;
+                }
+                else
+                {
+                    Console.WriteLine("Prime Number.");
+                    break;
+                }
+            }
+        }
+    }
+}
