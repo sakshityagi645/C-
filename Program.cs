8a05f9daa27c302c86c4c8e39994f2d55fbf88d0
Sakshi Tyagi <55343037+sakshityagi645@users.noreply.github.com>
2021-12-28 12:52:56 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace polymorphism
+{
+    class Program
+    {//function overloading
+        void print(int i)
+        {
+            Console.WriteLine("Integer number is: {0}", i);
+        }
+        void print(double f)
+        {
+            Console.WriteLine("Double number is: {0}", f);
+        }
+        void print(string s)
+        {
+            Console.WriteLine("String number is: {0}", s);
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+
+            obj.print(234);
+            obj.print(123.43);
+            obj.print("Hello Sakshi Tyagi");
+            Console.ReadLine();
+        }
+    }
+}
+
