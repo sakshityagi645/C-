a28e9c450a583a602a8a090db72c6fccbc472654
Sakshi Tyagi <55343037+sakshityagi645@users.noreply.github.com>
2021-12-28 13:10:40 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DynamicPolymorphism
+{
+    abstract class Shape
+    {
+        public abstract int area();
+    }
+    class Rectangle: Shape
+    {
+        private int length;
+        private int width;
+
+        public Rectangle(int a = 0, int b = 0)
+        {
+            length = a;
+            width = b;
+        }
+        public override int area()
+        {
+            Console.WriteLine("Rectangle area is: ");
+            return (width * length);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle(10, 6);
+            double a = r.area();
+            Console.WriteLine("Area: {0}", a);
+            Console.ReadLine();
+        }
+    }
+}
