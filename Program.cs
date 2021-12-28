692ebb6cdd41e20a5e199c02a7aa082fcd4d3d6b
Sakshi Tyagi <55343037+sakshityagi645@users.noreply.github.com>
2021-12-28 11:49:41 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Method_Overloading
+{
+    class Program
+    {
+        public int Add(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+        public int Add(int a, int b, int c)
+        {
+            int sum = a + b + c;
+            return sum;
+        }
+        public static void Main(string[] args)
+        {
+            Program obj = new Program();
+            int sum1 = obj.Add(123, 542);
+            Console.WriteLine(sum1);
+
+            int sum2 = obj.Add(143, 562, 543);
+            Console.WriteLine(sum2);
+            Console.ReadLine();
+        }
+    }
+}
